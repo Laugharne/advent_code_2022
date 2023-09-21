c89a4c630c0ffc711de4e6230d6638b1e4ef75de
laugharne <franck@maussand.net>
2023-09-21 16:53:20 +0200

    backup

--- day09_1/src/main.rs
+++ day09_1/src/main.rs
@@ -1,5 +1,9 @@
 use std::fs::read_to_string;
 
+struct Trace {
+	symbol:  char,
+	layer: u16,
+}
 
 fn main() {
 
@@ -10,12 +14,12 @@ fn main() {
 	let mut y_max: i32 = 0;
 
 	// Map dimensions
-	let mut map_nx: usize = 0;
-	let mut map_ny: usize = 0;
+	let map_nx: usize;
+	let map_ny: usize;
 
 	// Absolute value of start point position
-	let mut x_start: u32 = 0;
-	let mut y_start: u32 = 0;
+	let x_start: u32;
+	let y_start: u32;
 
 	println!();
 
@@ -83,13 +87,19 @@ fn main() {
 	println!("nx,ny = {},{}", map_nx, map_ny);
 	println!("ox,oy = {},{}", x_start, y_start);
 
-	struct Trace {
-		symbol:  char,
-		layer: u16,
-	}
+
 	let mut map: Vec<Vec<Trace>> = Vec::new();
 
-	for mx in 00..map_nx {
+	for my in 00..map_ny {
+		map.push(Vec::new());
+		for _x in 00..map_nx {
+			map[my].push( Trace {
+				symbol: '.',
+				layer: 0
+			});
+		}
+	}
+	/*for mx in 00..map_nx {
 		map.push(Vec::new());
 		for _y in 00..map_ny {
 			map[mx].push( Trace {
@@ -97,42 +107,93 @@ fn main() {
 				layer: 0
 			});
 		}
-	}
-
+	}*/
 
 	println!();
 
-	let nn_movements: usize = movements.len();
+	let nn_movements: i32 = movements.len() as i32;
+	let mut x_h: u32 = x_start;
+	let mut y_h: u32 = y_start;
+	let mut x_t: u32 = x_start;
+	let mut y_t: u32 = y_start;
 
-	for mv in 0..=nn_movements {
-		let mv1: i32 = (mv as i32) - 1;
-		if mv <= 0 {
-			println!("\n== Initial State ==\n");
+	for mv in -1..nn_movements {
+		if mv < 0 {
+			println!("== Initial State ==");
+			map[y_start as usize][x_start as usize].symbol = 's';
+			display_map( &map);
 		} else {
-			println!("\n== {} {} ==\n", movements[mv1 as usize].direction, movements[mv1 as usize].step);
-		}
+			let movement: &Move = &movements[mv as usize];
+			println!("== {} {} ==", movement.direction, movement.step);
+
+			let mut x_h_new = x_h;
+			let mut y_h_new = y_h;
+			let mut x_t_new = x_t;
+			let mut y_t_new = y_t;
+
+			for step in 0..movement.step {
+				match movement.direction {
+					'U' => { y_h_new -=1; },
+					'D' => { y_h_new +=1; },
+					'L' => { x_h_new -=1; },
+					'R' => { x_h_new +=1; },
+					_ => {},
+				}
+
+				// TODO
+				
+				map[y_h as usize][x_h as usize].symbol = '.';
+				map[y_t as usize][x_t as usize].symbol = '.';
+
+				map[y_start as usize][x_start as usize].symbol = 's';
+				map[y_t_new as usize][x_t_new as usize].symbol = 'T';  // `T` covers `s`
+				map[y_h_new as usize][x_h_new as usize].symbol = 'H';  // `H` covers `T` and `s`
+
+				x_h = x_h_new;
+				y_h = y_h_new;
+				x_t = x_t_new;
+				y_t = y_t_new;
 		
-		map[x_start as usize][y_start as usize] = Trace {
-			symbol: 's',
-			layer: 0
-		};
-
-		// TODO
-
-		for y in 0..map_ny {
-			for x in 0..map_nx {
-				let t = &map[x][y];
-				print!(" {}", t.symbol);
+				// Display trace `H` & `T`
+				display_map( &map);
+			
 			}
-			println!();
+
 		}
-	
-		println!();
-	
+
 	}
+
+	println!("\n== TRACE ==");
+	let mut visited: u32 = 0;
+
+	map.iter().for_each(|line| {
+		line.iter().for_each(|cell| {
+			let mark: char;	
+			match cell.layer {
+				0 => {mark = '.';},
+				_ => {mark = '#'; visited += 1;},
+			}
+			print!(" {}", mark);
+		});
+		println!();
+	});
+
+	println!();
+	println!("Visited positions = {}", visited);
+	println!();
 
 }
 
+
+fn display_map(map: &Vec<Vec<Trace>>) {
+	map.iter().for_each(|line| {
+		line.iter().for_each(|cell| {
+			print!(" {}", cell.symbol);
+		});
+		println!();
+	});
+	println!();
+}
 
 
 // https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
